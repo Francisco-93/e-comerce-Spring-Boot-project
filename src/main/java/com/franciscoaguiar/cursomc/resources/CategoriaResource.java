127216b8ad45d6bc901c85b874bbb4b8e79df487
Francisco-93 <francisco.fro.agui@gmail.com>
2020-11-10 10:52:10 -0300

    REST testado

---
+++ src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.franciscoaguiar.cursomc.resources;
+
+import java.io.Serializable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando";
+	}
+}
