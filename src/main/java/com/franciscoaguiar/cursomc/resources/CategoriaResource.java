7f0cc91816b5d1ce9c341c8d52d162b7fbf5a93b
Francisco-93 <francisco.fro.agui@gmail.com>
2020-11-10 13:38:28 -0300

    Criando operacao de instanciacao

--- src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
@@ -1,19 +1,35 @@
 package com.franciscoaguiar.cursomc.resources;
 
 import java.io.Serializable;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.franciscoaguiar.cursomc.domain.Categoria;
+import com.franciscoaguiar.cursomc.services.CategoriaService;
+
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping
 public class CategoriaResource implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping (value = "/categorias")
+	public ResponseEntity<List<Categoria>> findAll(){
+		return ResponseEntity.ok().body(categoriaService.findAll());
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(categoriaService.findById(id));
+	}
+	
 }
---
+++ src/main/java/com/franciscoaguiar/cursomc/services/CategoriaService.java
@@ -0,0 +1,30 @@
+package com.franciscoaguiar.cursomc.services;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.franciscoaguiar.cursomc.domain.Categoria;
+import com.franciscoaguiar.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
+	}
+	
+	public Categoria findById(Integer id){
+		Categoria obj = categoriaRepository.findById(id).get();
+		return obj;
+	}
+
+}
