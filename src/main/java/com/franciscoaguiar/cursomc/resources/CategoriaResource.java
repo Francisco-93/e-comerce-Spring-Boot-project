74ffb34b763f7d4cc37089dc04dd4d753e89dfbc
Francisco-93 <francisco.fro.agui@gmail.com>
2020-11-12 14:33:28 -0300

    Listando todas as /categorias com DTO

--- src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/franciscoaguiar/cursomc/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.franciscoaguiar.cursomc.resources;
 import java.io.Serializable;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.franciscoaguiar.cursomc.domain.Categoria;
+import com.franciscoaguiar.cursomc.dto.CategoriaDTO;
 import com.franciscoaguiar.cursomc.services.CategoriaService;
 
 @RestController
@@ -29,8 +31,10 @@ public class CategoriaResource implements Serializable{
 	private CategoriaService categoriaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Categoria>> findAll(){
-		return ResponseEntity.ok().body(categoriaService.findAll());
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@GetMapping(value = "/{id}")
