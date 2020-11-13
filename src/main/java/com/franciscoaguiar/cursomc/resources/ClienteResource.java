22989bdc3a80fe7af52d984a8cfa3a77dfc28132
Francisco-93 <francisco.fro.agui@gmail.com>
2020-11-13 15:17:41 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/franciscoaguiar/cursomc/resources/ClienteResource.java
+++ src/main/java/com/franciscoaguiar/cursomc/resources/ClienteResource.java
@@ -1,16 +1,28 @@
 package com.franciscoaguiar.cursomc.resources;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.franciscoaguiar.cursomc.domain.Cliente;
+import com.franciscoaguiar.cursomc.dto.ClienteDTO;
 import com.franciscoaguiar.cursomc.services.ClienteService;
 
 @RestController
@@ -20,17 +32,54 @@ public class ClienteResource implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Autowired
-	private ClienteService categoriaService;
+	private ClienteService clienteService;
 	
 	@GetMapping
-	public ResponseEntity<List<Cliente>> findAll(){
-		return ResponseEntity.ok().body(categoriaService.findAll());
+	public ResponseEntity<List<ClienteDTO>> findAll(){
+		List<Cliente> cliente = clienteService.findAll();
+		List<ClienteDTO> clienteDTO = cliente.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(clienteDTO);
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id){
-		Cliente obj = categoriaService.findById(id);
+		Cliente obj = clienteService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO){
+		Cliente obj = clienteService.fromDTO(objDTO);
+		obj = clienteService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> updateById(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = clienteService.fromDTO(objDTO);
+		obj.setId(id);
+		obj = clienteService.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Integer id){
+		clienteService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(path = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value="page", defaultValue="0") Integer page, 
+														@RequestParam(value="linesPage", defaultValue="24")Integer linesPage, 
+														@RequestParam(value="orderBy", defaultValue="nome")String orderBy, 
+														@RequestParam(value="direction", defaultValue="ASC")String direction){
+		Page<Cliente> list = clienteService.findPage(page, linesPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
--- src/main/java/com/franciscoaguiar/cursomc/services/ClienteService.java
+++ src/main/java/com/franciscoaguiar/cursomc/services/ClienteService.java
@@ -5,10 +5,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.franciscoaguiar.cursomc.domain.Cliente;
+import com.franciscoaguiar.cursomc.dto.ClienteDTO;
 import com.franciscoaguiar.cursomc.repositories.ClienteRepository;
+import com.franciscoaguiar.cursomc.services.exceptions.DataIntegrityException;
 import com.franciscoaguiar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -35,5 +41,40 @@ public class ClienteService implements Serializable{
 		}
 		return obj.get();
 	}*/
+	
+	public Cliente insert (Cliente obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+	
+	public Cliente update (Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return categoriaRepository.save(newObj);
+	}
+	
+	public void deleteById(Integer id) {
+		findById(id);
+		try {
+			categoriaRepository.deleteById(id);			
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData (Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 
 }
